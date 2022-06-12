d3ea418644bbfef912f9afd6a5dd82867b2c10a2
Klevest Palucaj <klevestpalucaj@gmail.com>
2022-06-12 13:57:08 +0300

    language flags linked to firebase

--- client/src/components/UI/LanguageSwitcher/LanguageSwitcher.js
+++ client/src/components/UI/LanguageSwitcher/LanguageSwitcher.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import i18next from "i18next";
+import { imageGetter } from "../../../Helpers/Const/constants";
 
 import "./LanguageSwitcher.css";
 
 const LanguageSwitcher = () => {
+  const [greeceFlag, setGreeceFlag] = useState("");
+  const [englishFlag, setEnglishFlag] = useState("");
+
+  useEffect(() => {
+    const exec = async () => {
+      const { myArr: greeceResult } = await imageGetter(
+        [{ image: "greece.png" }],
+        "Flags/",
+        true
+      );
+      
+      const { myArr: englishResult } = await imageGetter(
+        [{ image: "united-kingdom.png" }],
+        "Flags/",
+        true
+      );
+      setGreeceFlag(greeceResult[0].image);
+      setEnglishFlag(englishResult[0].image);
+    };
+    exec();
+  }, []);
+
   const changeLanguage = (lng) => {
     i18next.changeLanguage(lng);
   };
@@ -11,12 +34,12 @@ const LanguageSwitcher = () => {
   return (
     <div className="language-wrapper">
       <img
-        src={`${process.env.REACT_APP_IMAGES_URL}/Images/Flags/greece.png`}
+        src={greeceFlag}
         alt="Greek flag"
         onClick={() => changeLanguage("el")}
       />
       <img
-        src={`${process.env.REACT_APP_IMAGES_URL}/Images/Flags/united-kingdom.png`}
+        src={englishFlag}
         alt="English flag"
         onClick={() => changeLanguage("en")}
       />
